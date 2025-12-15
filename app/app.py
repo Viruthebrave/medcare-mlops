7bd8ff9161e9b5ee47eedb99e3bf630ed67715ee
unknown <your_email@example.com>
2025-12-15 11:51:48 +0530

    test: auto deploy version v3

--- app/app.py
+++ app/app.py
@@ -1,25 +1,19 @@
 import sys
 import os
-from datetime import datetime
-
-# Add project root to PYTHONPATH
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(ROOT_DIR)
-
 from flask import Flask, request, jsonify
 from src.predict import predict_medicine
 
-app = Flask(__name__)
-
+# 🔥 CHANGE VERSION HERE EACH TEST
 APP_VERSION = "v3"
-DEPLOYED_AT = datetime.utcnow().isoformat()
+
+app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({
         "service": "MedCare MLOps API",
+        "status": "RUNNING",
         "version": APP_VERSION,
-        "deployed_at": DEPLOYED_AT,
         "endpoints": {
             "health": "/health",
             "predict": "/predict (POST)"
@@ -30,14 +24,12 @@ def home():
 def health():
     return jsonify({
         "status": "UP",
-        "version": APP_VERSION,
-        "deployed_at": DEPLOYED_AT
+        "version": APP_VERSION
     })
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-
     if not data or "disease" not in data:
         return jsonify({"error": "Please provide disease name"}), 400
 
